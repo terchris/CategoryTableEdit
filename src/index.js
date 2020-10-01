f8771fb47ce508bee5a4be51037ce98e004dae1a
Terje Christensen <terchris@users.noreply.github.com>
2020-10-01 11:11:54 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+import "./index.css";
+
+function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input name="firstName" placeholder="bill" ref={register} />
+        </div>
+
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input name="lastName" placeholder="luo" ref={register} />
+        </div>
+
+        <div>
+          <label htmlFor="isDeveloper">Is an developer?</label>
+          <input
+            type="checkbox"
+            name="isDeveloper"
+            placeholder="luo"
+            value="yes"
+            ref={register}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            placeholder="bluebill1049@hotmail.com"
+            type="email"
+            ref={register}
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
